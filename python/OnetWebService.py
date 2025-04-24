f7249af127d162075adf2612cc96bb74da161ae4
Hopper262 <hopper@whpress.com>
2025-04-24 13:56:44 -0400

    Improve Python error handling

--- python/OnetWebService.py
+++ python/OnetWebService.py
@@ -19,22 +19,35 @@ class OnetWebService:
             self._url_root = 'https://api-v' + version + '.onetcenter.org/'
     
     def call(self, path, *query):
-        url = self._url_root + path
-        if len(query) > 0:
-            url += '?' + urllib.parse.urlencode(query, True)
-        req = urllib.request.Request(url, None, self._headers)
-        handle = None
         try:
-            handle = urllib.request.urlopen(req)
-        except urllib.error.HTTPError as e:
-            if e.code == 422:
-                return json.load(e)
-            else:
-                return { 'error': 'Call to ' + url + ' failed with error code ' + str(e.code) }
-        except urllib.error.URLError as e:
-            return { 'error': 'Call to ' + url + ' failed with reason: ' + str(e.reason) }
-        code = handle.getcode()
-        if (code != 200) and (code != 422):
-            return { 'error': 'Call to ' + url + ' failed with error code ' + str(code),
-                     'urllib2_info': handle }
-        return json.load(handle)
+            url = self._url_root + path
+            if len(query) > 0:
+                url += '?' + urllib.parse.urlencode(query, True)
+            req = urllib.request.Request(url, None, self._headers)
+            handle = None
+            try:
+                handle = urllib.request.urlopen(req)
+            except urllib.error.HTTPError as e:
+                if e.code == 422:
+                    try:
+                        return json.load(e)
+                    except json.JSONDecodeError:
+                        return { 'error': 'Call to ' + url + ' failed to return valid JSON' }
+                    except UnicodeDecodeError:
+                        return { 'error': 'Call to ' + url + ' failed to return valid UTF-8' }
+                else:
+                    return { 'error': 'Call to ' + url + ' failed with error code ' + str(e.code) }
+            except urllib.error.URLError as e:
+                return { 'error': 'Call to ' + url + ' failed with reason: ' + str(e.reason) }
+            code = handle.getcode()
+            if (code != 200) and (code != 422):
+                return { 'error': 'Call to ' + url + ' failed with error code ' + str(code),
+                        'urllib2_info': handle }
+            try:
+                return json.load(handle)
+            except json.JSONDecodeError:
+                return { 'error': 'Call to ' + url + ' failed to return valid JSON' }
+            except UnicodeDecodeError:
+                return { 'error': 'Call to ' + url + ' failed to return valid UTF-8' }
+        except Exception as e:
+            return { 'error': 'Call failed with unexpected error', 'exception': e }
